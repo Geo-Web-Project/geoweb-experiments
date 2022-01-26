5c17a91964d1298a70992d9c80fe865c2042a51a
10dimensions <relay.10d@gmail.com>
2022-01-26 13:41:30 +0530

    Test DOM Append

--- 04-ar-location-based/js/script.js
+++ 04-ar-location-based/js/script.js
@@ -1,5 +1,6 @@
 
 var test_model_src = 'https://gateway.pinata.cloud/ipfs/QmTxmmG33T1mkEucgzDNQLDEQUxDsymusyRv7UiqESnq9F'
+var scale = 0.25
 
 window.onload = () => {
     const scene = document.querySelector('a-scene');
@@ -15,18 +16,30 @@ window.onload = () => {
         const latitude = place.location.lat;
         const longitude = place.location.lng;
 
+        scene.innerHTML += 
+              `<a-entity
+                    animation-mixer="loop: repeat"
+                    gltf-model=${test_model_src}
+                    scale="${scale} ${scale} ${scale}"
+                    gps-entity-place="latitude: ${latitude}; longitude: ${longitude};"
+                    title=${place.name}
+                ></a-entity>
+              `
+
         // add place name
+        /*
         const model = document.createElement('a-entity');
+        model.setAttribute('animation-mixer', 'loop: repeat');
         model.setAttribute('gltf-model', test_model_src); //'./assets/hot_air_balloon.glb'
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('title', place.name);
-        model.setAttribute('scale', '15 15 15');
+        model.setAttribute('scale', '0.25 0.25 0.25');
         
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
         });
 
         scene.appendChild(model);
+        console.log(model);
+        */
     });
             
 
