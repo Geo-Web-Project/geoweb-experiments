87e48c1d80422b6ac6244085b3eb17a3ec49e70a
10dimensions <relay.10d@gmail.com>
2022-02-08 10:57:56 +0530

    Push latest

--- 04-ar-location-based/js/script.js
+++ 04-ar-location-based/js/script.js
@@ -1,44 +1,51 @@
 
-var test_model_src = 'https://gateway.pinata.cloud/ipfs/QmTxmmG33T1mkEucgzDNQLDEQUxDsymusyRv7UiqESnq9F'
-var scale = 0.25
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+
+//var glb_cid = 'https://gateway.pinata.cloud/ipfs/QmTxmmG33T1mkEucgzDNQLDEQUxDsymusyRv7UiqESnq9F'
+//var scale = 1
+//var altitude = 100
+//{ name: "Hot Air Balloon", location: { lng: 80.072225, lat: 12.823755 } }
+
+let glb_cid = params.glb_cid.toString();
+let scale = parseFloat(params.scale);
+
+let lat = parseFloat(params.lat);
+let lon = parseFloat(params.lon);
+let altitude = parseFloat(params.alt);
+
+let place = { name: params.name.toString(), location: { lng: lon, lat: lat } }
 
 window.onload = () => {
     const scene = document.querySelector('a-scene');
     const dom = document.querySelector('body');
 
-    //<a-entity gltf-model="./assets/hot_air_balloon.glb" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: 12.823890; latitude: 80.074987;" animation-mixer/>
+    const latitude = place.location.lat;
+    const longitude = place.location.lng;
 
+    dom.innerHTML += 
+        `
+        <a-scene
+        renderer="logarithmicDepthBuffer: true;"
+        loading-screen="enabled: false;"
+        arjs="sourceType: webcam; debugUIEnabled: false;"
+        embedded>
 
-    let places = [
-        { name: "Hot Air Balloon", location: { lat: 80.074987, lng: 12.823890 } },
-    ]
-
-    places.forEach((place) => {
-        const latitude = place.location.lat;
-        const longitude = place.location.lng;
-
-        debugger
-        dom.innerHTML += 
-              `
-              <a-scene
-                renderer="logarithmicDepthBuffer: true;"
-                loading-screen="enabled: false;"
-                arjs="sourceType: webcam; debugUIEnabled: false;"
-                embedded>
-
-                <a-entity
-                    animation-mixer="loop: repeat"
-                    gltf-model=${test_model_src}
-                    scale="${scale} ${scale} ${scale}"
-                    gps-entity-place="latitude: ${latitude}; longitude: ${longitude};"
-                    title=${place.name}
-                ></a-entity>
-                <a-camera gps-camera rotation-reader></a-camera>
-                </a-scene>
-              `
+        <a-entity
+            animation-mixer="loop: repeat"
+            gltf-model=${glb_cid}
+            scale="${scale} ${scale} ${scale}"
+            position="0 ${altitude} 0"
+            gps-entity-place="latitude: ${latitude}; longitude: ${longitude};"
+            title=${place.name}
+        ></a-entity>
+        <a-camera gps-camera rotation-reader></a-camera>
+        </a-scene>
+        `
 
         // add place name
-        const model = document.createElement('a-entity');
+        //const model = document.createElement('a-entity');
 
         /*
         model.setAttribute('animation-mixer', 'loop: repeat');
@@ -66,9 +73,6 @@ window.onload = () => {
         model.setAttributeNode(scale);
 
         scene.appendChild(model);
-        */
-        
-    });
-            
+        */    
 
 };
